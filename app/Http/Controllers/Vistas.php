72b54a70f86207b70199201ce1b14fb02d0eee39
LuisMasCreativo <luismascreativo@gmail.com>
2023-05-24 12:06:41 -0600

    Vista inicio y agregar Estudiantes

---
+++ app/Http/Controllers/Vistas.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class Vistas extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth'])->only(['index']);
+    }
+
+    public function index(){
+        $titulo = "Inicio";
+        return view('index', compact('titulo'));
+    }
+
+    public function registrar(Request $request)
+    {
+        $items = new User();
+        $items->nombre = $request->nombre;
+        $items->apellido_paterno = $request->apellido_paterno;
+        $items->apellido_materno = $request->apellido_materno;
+        $items->nombre_usuario = $request->nombre_usuario;
+        $items->email = $request->email;
+        $items->password = Hash::make($request->password);
+        $items->save();
+        return redirect()->route('auth-login');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Vistas;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(Vistas::class)->group(function (){
+    Route::get('/inicio', 'index')->name('vistas-inicio');
+    Route::post('/store', 'registrar')->name('vistas-registro');
+});
+
+Route::controller(AuthController::class)->group(function (){
+    Route::get('/', 'login')->name('auth-login');
+    Route::post('/signIn', 'logear')->name('auth-logear');
+    Route::get('/logout', 'logout')->name('auth-logout');
+});
+
