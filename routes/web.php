d2a8f4972bff6f15338f199a34a06573c84d478f
LuisMasCreativo <luismascreativo@gmail.com>
2023-05-24 12:04:53 -0600

    Vista inicio y agregar Estudiantes

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Vistas;
-use Illuminate\Support\Facades\Route;
-
-Route::controller(Vistas::class)->group(function (){
-    Route::get('/','login') -> name('vistas-login');
-    Route::get('/inicio','inicio') -> name('vista-inicio');
-});
